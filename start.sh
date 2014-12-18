58484d1c7c326fdce41c7cc2db0f143a4473752a
Jared Markell <jaredm4@gmail.com>
2014-12-18 08:06:13 -0800

    snapshot..

--- start.sh
+++ start.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
-# Give server time to boot, then setup proxy on port 9091
-(sleep 3 && curl -sSX POST -d "port=9091&bindAddress=$(cat /etc/hosts | grep $(hostname) | cut -f1)" http://0.0.0.0:9090/proxy) &
+#ADDRESS=$(cat /etc/hosts | grep $(hostname) | cut -f1)
+ADDRESS=0.0.0.0
 
+
+# Give server time to boot, then setup proxy on port 9091
+(sleep 3 && curl -sSX POST -d "port=9091&bindAddress=${ADDRESS}" http://${ADDRESS}:9090/proxy) &
+
+# -address isn't working here
 exec /browsermob-proxy-2.0-beta-9/bin/browsermob-proxy -port 9090
